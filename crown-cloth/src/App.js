cc2d1ecadc86fba01fe4360097e57630106526c2
maniraj <maniraj@yahoo.com>
2021-01-06 12:38:45 +0530

    check git rulemas

--- crown-cloth/src/App.js
+++ crown-cloth/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editmani<code>src/App.js</code> and save to reload.
+          Editmani1<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
