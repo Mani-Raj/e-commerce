7c0d97aeea773410d916ef5410694f3bcb6738ca
Mani-Raj <smaniraj14@gmail.com>
2021-01-11 22:52:23 +0530

    react router dom i

--- crown-cloth/src/App.js
+++ crown-cloth/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
-import HomePage from './page/homepage/homepage.component'
+import HomePage from './page/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const Has = () => (
+  <h1>Maniraj</h1>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/mani' component={Has} />
+      </Switch>
     </div>
   );
 }
