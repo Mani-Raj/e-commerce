562a3d523ecb30f072cfd64ad8c470cc674267ca
Mani-Raj <smaniraj14@gmail.com>
2021-01-17 20:28:28 +0530

    implement url for items, using withRouter

--- crown-cloth/src/App.js
+++ crown-cloth/src/App.js
@@ -1,18 +1,23 @@
 
 import './App.css';
 import HomePage from './page/homepage/homepage.component';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 
-const Has = () => (
-  <h1>Maniraj</h1>
+const Hats = (props) => { 
+  console.log('hats called ', props)
+  return (
+  <div>
+    <h1>HATS</h1>
+  </div>
 )
+  }
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/mani' component={Has} />
+        <Route exact path='/' component={HomePage} />        
+        <Route exact path='/shop/hats' component = {Hats} />
       </Switch>
     </div>
   );
